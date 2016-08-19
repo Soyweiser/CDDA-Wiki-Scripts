43348bdf7120e7fdade1e7ba6018aeddd5fbaf33
Soyweiser <Soyweiser@gmail.com>
2016-08-19 03:27:24 +0200

    Added script that converts materials.json to materials for the wiki.

---
+++ materials.py
@@ -0,0 +1,42 @@
+import json
+from pprint import pprint
+from Tkinter import Tk
+root = Tk()
+root.withdraw()
+
+#data is copied to clipboard
+
+with open('data/json/materials.json') as data_file:    
+    data = json.load(data_file)
+
+output = [ "" ]
+for it in range(0, len(data)):
+    output.append("{{row/{{PAGENAME}}|mat=")
+    output.append(data[it]['ident'])
+    output.append("|name=")
+    output.append(data[it]['name'])
+    output.append("|bashdv=")
+    output.append(data[it]['bash_dmg_verb'])
+    output.append("|cutdv=")
+    output.append(data[it]['cut_dmg_verb'])
+    output.append("|dmgadj1=")
+    output.append(data[it]['dmg_adj'][0])
+    output.append("|dmgadj2=")
+    output.append(data[it]['dmg_adj'][1])
+    output.append("|dmgadj3=")
+    output.append(data[it]['dmg_adj'][2])
+    output.append("|dmgadj4=")
+    output.append(data[it]['dmg_adj'][3])
+    output.append("|density=")
+    output.append(str(data[it]['density']))
+        
+    output.append("}}\n\n")
+
+text = "".join(output)
+text.replace("\n", "\\n")
+print text
+root.clipboard_clear()
+root.clipboard_append(text)
+root.update()
+root.destroy()
+exit()
