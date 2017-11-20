66180508badb2828fc4975ea8f2ba681f8a5034d
Soyweiser <Soyweiser@gmail.com>
2017-11-20 21:50:00 +0100

    Added more missing data to the bionics script. It should be mostly complete now.

--- bionics.py
+++ bionics.py
@@ -100,7 +100,7 @@ while True:
                 output.append("\n|weight=")
                 output.append(str(getValue(item_id,'weight')))
                 output.append("\n|b_name=")
-                output.append( ID_To_String(data1[item_id]['id']) )
+                output.append(ID_To_String(data1[item_id]['id']))
                 output.append("\n|difficulty=")
                 output.append(str(getValue(item_id,'difficulty')))
                 if('capacity' in data[var]):
@@ -153,8 +153,58 @@ while True:
                 
                 #footer
                 output.append("""\n}}<noinclude>
-==Notes==
-<!-- *YOUR PERSONAL NOTES AND HINTS GO BELOW HERE* -->
+==Notes==""")
+                if('occupied_bodyparts' in data[var]):
+                    output.append("\n* Uses the following [[Bionics#Bionic_Slots|bionic slot(s)]]:\n")
+                    for it in range(0, len(data[var]["occupied_bodyparts"])):
+                        output.append("** {{btt|")
+                        output.append(str(data[var]["occupied_bodyparts"][it][0]))
+                        output.append("}} ")
+                        output.append(str(data[var]["occupied_bodyparts"][it][1]))
+                        output.append(".\n")
+                if('faulty' in data[var]):
+                    output.append("* This is considered a [[Bionics#Malfunctioning_bionics|malfunctioning bionic]].\n")
+                if('capacity' in data[var]):
+                    output.append("* This bionic adds ")
+                    output.append(str(data[var]['capacity']))
+                    output.append(" capacity.\n")
+                if('power_source' in data[var]):
+                    output.append("* This bionic a source of power. The actual type of power source depends on the item. Could be solar, kinetic, atomics, or anything else.\n")
+                if('toggled' in data[var] or 'act_cost' in data[var]):
+                    output.append("* This bionic can be turned on, which might have an power cost.\n")
+                if('react_cost' in data[var]):
+                    output.append("* This bionic can react automatically, which costs ")
+                    output.append(str(data[var]['react_cost']))
+                    output.append(" power units.\n")
+                if('included_bionics' in data[var]):
+                    output.append("* Installing this bionic also gives the")
+                    for it in range(0, len(data[var]["included_bionics"])):
+                        if (it > 0):
+                            output.append(",")
+                        output.append(" [[")
+                        output.append(ID_To_String(data[ID_To_Bio_Int(str(data[var]['included_bionics'][it]))]['id']))
+                        output.append("]]")
+                    output.append(" bionic")
+                    if (len(data[var]["included_bionics"]) > 0):
+                        output.append("s")
+                    output.append(".\n")
+                if('fake_item' in data[var]):
+                    output.append("* This gives the use of the following 'fake item' [http://cdda-trunk.chezzo.com/")
+                    output.append(str(data[var]['fake_item']))
+                    output.append(" ")
+                    output.append(str(data[var]['fake_item']))
+                    output.append("]")
+                    if('toggled' in data[var] or 'act_cost' in data[var]):
+                        output.append(" when activated")
+                    output.append(".\n")
+                if('gun_bionic' in data[var]):
+                    output.append("* This bionic counts as a gun.\n")
+                if('weapon_bionic' in data[var]):
+                    output.append("* This bionic counts as a weapon.\n")
+                if('time' in data[var]):
+                    output.append("* This bionic has an over time effect.\n")
+
+                output.append("""<!-- *YOUR PERSONAL NOTES AND HINTS GO BELOW HERE* -->
 
 [[Category:CBMs]]
 {{footer/CBM}}
