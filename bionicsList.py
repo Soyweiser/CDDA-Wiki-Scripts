2d193d435b030beb6d2eaa843c6094d1e453eb70
Soyweiser <Soyweiser@gmail.com>
2018-02-07 17:31:06 +0100

    * Fix for the microreactor upgrade CBM.

--- bionicsList.py
+++ bionicsList.py
@@ -7,8 +7,11 @@ import pywikibot
 #Usage: python [location of pywikibotinstall]\pwb.py bionicsList.py
 #   Then input your password, and wait for the pages to be updated.
 
-with open('data/json/bionics.json') as data_file:    
+with open('data/json/bionics.json') as data_file:
     data = json.load(data_file)
+
+with open('data/json/items/bionics.json') as data_file:
+    data1 = json.load(data_file)
 
 ID_bionic = list()
 ID_bionic_faulty = list()
@@ -20,6 +23,16 @@ for iterator in range(0, len(data)):
             ID_bionic.append(data[iterator]["name"])
     else:
         ID_bionic.append(data[iterator]["name"])
+
+for iterator in range(0, len(data1)): #the previous loop does not add some cbms, this loop will add the missing ones.
+    if(not 'abstract' in data1[iterator]): #don't add abstract bionics.
+        excluded = True
+        for iter in range(0, len(data)):
+            if(data[iter]['id'] == data1[iterator]['id']):
+                excluded = False
+                break
+        if(excluded):
+            ID_bionic.append(data1[iterator]["name"])
 
 ID_bionic = sorted(ID_bionic, key=string.lower) 
 ID_bionic_faulty = sorted(ID_bionic_faulty, key=string.lower) 
