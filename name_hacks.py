8292cb66c14b475b21f486584d9b87b2009b673c
Soyweiser <Soyweiser@gmail.com>
2018-10-20 13:40:40 +0200

    * added breather spawn name hack.

--- name_hacks.py
+++ name_hacks.py
@@ -1,5 +1,5 @@
 #the following dict is made for monster id's and specific wiki page names. For example the 'mon_dog_thing' has as a page name 'Dog (nether)', but the links are called 'dog' for spoilers sake. The second hacks is for the Template:Enemiestable pages.
-monster_name_hacks = { 'mon_dog_thing' : 'Dog (nether)|Dog', 'mon_triffid_heart' : 'Triffid heart (creature)|Triffid heart' }
+monster_name_hacks = { 'mon_dog_thing' : 'Dog (nether)|Dog', 'mon_triffid_heart' : 'Triffid heart (creature)|Triffid heart', 'mon_breather' : 'Breather (spawn)|Breather spawn' }
 
 def monster_name(x): #checks if the id x is in the monster_name_hack list, and if it is it returns the correct hack name. If it isn't it returns the ID.
     if(x in monster_name_hacks):
